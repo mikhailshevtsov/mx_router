10263506bfda7a814624d5b0b9d7a33ffc714545
mikhailshevtsov <mikhail.shevtsoff@gmail.com>
2026-08-15 13:44:59 +0200

    iterators version revision

--- include/mx/router.hpp
+++ include/mx/router.hpp
@@ -7,10 +7,7 @@
 #include <memory>
 #include <functional>
 #include <optional>
-#include <type_traits>
 #include <utility>
-#include <vector>
-#include <algorithm>
 
 namespace mx
 {
@@ -25,171 +22,249 @@ public:
 private:
     struct node
     {
-        std::optional<value_t> _value;
-        std::unordered_map<std::string, std::shared_ptr<node>> _children;
+        std::optional<value_t> value;
+        std::unordered_map<std::string, std::shared_ptr<node>> children;
 
-        std::shared_ptr<node> at(std::string_view path);
-        std::shared_ptr<node> at(std::string_view path, const on_path_param_t& on_path_param) const;
+        std::shared_ptr<node> insert(std::string_view path);
 
-        std::shared_ptr<node> mount(std::string_view path, std::shared_ptr<node> other);
-        std::shared_ptr<node> unmount(std::string_view path);
+        std::shared_ptr<node> find(std::string_view path, const on_path_param_t& on_path_param = {});
+        std::shared_ptr<const node> find(std::string_view path, const on_path_param_t& on_path_param = {}) const;
+
+        std::shared_ptr<node> insert(std::string_view path, std::shared_ptr<node> other);
+        std::shared_ptr<node> remove(std::string_view path);
     };
+
+    router(std::shared_ptr<node> _node);
 
     static const char* find_first_of(const char* data, const char* chars);
 
 private:
-    router(std::shared_ptr<node> root);
+    template <bool IsConst>
+    class basic_iterator
+    {
+    private:
+        using node_t = std::conditional_t<IsConst, const node, node>;
+        using ref_t = std::conditional_t<IsConst, const std::optional<value_t>&, std::optional<value_t>&>;
+        using ptr_t = std::conditional_t<IsConst, const std::optional<value_t>*, std::optional<value_t>*>;
+
+    public:
+        basic_iterator();
+        basic_iterator(std::nullptr_t);
+
+        ref_t operator*() const;
+        ptr_t operator->() const;
+
+        explicit operator bool() const;
+
+        bool operator==(const basic_iterator& other) const;
+        bool operator!=(const basic_iterator& other) const;
+
+    private:
+        friend class router;
+        basic_iterator(std::shared_ptr<node_t> _node);
+
+    private:
+        std::shared_ptr<node_t> _node;
+    };
+
+public:
+    using iterator = basic_iterator<false>;
+    using const_iterator = basic_iterator<true>;
 
 public:
     router();
+    router(iterator it);
 
-    std::optional<value_t>& at(std::string_view path);
-    const std::optional<value_t>& at(std::string_view path, const on_path_param_t& on_path_param = {}) const;
+    iterator insert(std::string_view path);
 
-    router mount(std::string_view path, router other);
-    router unmount(std::string_view path);
+    iterator find(std::string_view path, const on_path_param_t& on_path_param = {});
+    const_iterator find(std::string_view path, const on_path_param_t& on_path_param = {}) const;
+
+    iterator insert(std::string_view path, iterator other);
+    iterator remove(std::string_view path);
 
     bool contains(std::string_view path) const;
 
+    iterator root();
+    const_iterator root() const;
+
     void clear();
     bool empty() const;
-    explicit operator bool() const;
+    operator bool() const;
 
 private:
     std::shared_ptr<node> _root;
 };
 
 template <typename T>
-std::shared_ptr<typename router<T>::node> router<T>::node::at(std::string_view path)
+std::shared_ptr<typename router<T>::node> router<T>::node::insert(std::string_view path)
 {
     const char* begin = path.size() > 0 ? path.data() + 1 : path.data();
     const char* end = find_first_of(begin, "/?");
     std::string next_word(begin, end - begin);
 
-    auto it = _children.find(next_word);
+    auto it = children.find(next_word);
 
-    if (it == std::end(_children))
-        it = _children.emplace(next_word, std::make_shared<node>()).first;
+    if (it == std::end(children))
+        it = children.emplace(next_word, std::make_shared<node>()).first;
 
     if (*end == 0 || *end == '?')
         return it->second;
 
-    return it->second->at(end);
+    return it->second->insert(end);
 }
 
 template <typename T>
-std::shared_ptr<typename router<T>::node> router<T>::node::at(std::string_view path, const on_path_param_t& on_path_param) const
+std::shared_ptr<typename router<T>::node> router<T>::node::find(std::string_view path, const on_path_param_t& on_path_param)
 {
     const char* begin = path.size() > 0 ? path.data() + 1 : path.data();
     const char* end = find_first_of(begin, "/?");
     std::string next_word(begin, end - begin);
 
-    auto it = _children.find(next_word);
-    
-    auto find_wildcard = [&]()
+    auto it = children.find(next_word);
+
+    if (it != std::end(children))
     {
-        auto it = std::find_if(std::begin(_children), std::end(_children), [](const auto& kv) { return !kv.first.empty() && kv.first[0] == '*'; });
-        if (it != std::end(_children))
+        if (*end == 0 || *end == '?')
         {
-            const char* end = find_first_of(begin, "?");
+            if (!it->second->value)
+                return {};
+            return it->second;
+        }
+        auto _node = it->second->find(end, on_path_param);
+        if (_node)
+            return _node;
+    }
+
+    it = std::begin(children);
+    for (it = std::find_if(it, std::end(children), [](const auto& kv) { return !kv.first.empty() && kv.first[0] == ':'; }); it != std::end(children); ++it)
+    {
+        if (*end == 0 || *end == '?')
+        {
+            if (!it->second->value)
+                return {};
             std::string_view param(it->first.data() + 1, it->first.size() - 1);
             if (!param.empty() && on_path_param)
-                on_path_param(param, std::string_view(begin, end - begin));
+                on_path_param(param, next_word);
+            return it->second;
         }
-        return it;
-    };
 
-    if (it == std::end(_children))
-    {
-        it = std::begin(_children);
-        for (it = std::find_if(it, std::end(_children), [](const auto& kv) { return !kv.first.empty() && kv.first[0] == ':'; }); it != std::end(_children); ++it)
+        auto _node = it->second->find(end, on_path_param);
+        if (_node)
         {
-            if (*end == 0 || *end == '?')
-            {
-                if (!it->second->_value)
-                    return {};
-                std::string_view param(it->first.data() + 1, it->first.size() - 1);
-                if (!param.empty() && on_path_param)
-                    on_path_param(param, next_word);
-                return it->second;
-            }
-  
-            auto _node = it->second->at(end, on_path_param);
-            if (_node)
-            {
-                std::string_view param(it->first.data() + 1, it->first.size() - 1);
-                if (!param.empty() && on_path_param)
-                    on_path_param(param, next_word);
-                return _node;
-            }
+            std::string_view param(it->first.data() + 1, it->first.size() - 1);
+            if (!param.empty() && on_path_param)
+                on_path_param(param, next_word);
+            return _node;
         }
     }
 
-    if (it == std::end(_children))
-        it = find_wildcard();
-    if (it == std::end(_children))
+    it = std::find_if(std::begin(children), std::end(children), [](const auto& kv) { return !kv.first.empty() && kv.first[0] == '*'; });
+    if (it == std::end(children) || !it->second->value)
         return {};
-
-    if (*end == 0 || *end == '?')
-    {
-        if (!it->second->_value)
-            return {};
-        return it->second;
-    }
-
-    auto _node = it->second->at(end, on_path_param);
-    if (_node)
-        return _node;
-
-    it = find_wildcard();
-    if (it == std::end(_children))
-        return {};
-
+    end = find_first_of(begin, "?");
+    std::string_view param(it->first.data() + 1, it->first.size() - 1);
+    if (!param.empty() && on_path_param)
+        on_path_param(param, std::string_view(begin, end - begin));
     return it->second;
 }
 
 template <typename T>
-std::shared_ptr<typename router<T>::node> router<T>::node::mount(std::string_view path, std::shared_ptr<node> other)
+std::shared_ptr<const typename router<T>::node> router<T>::node::find(std::string_view path, const on_path_param_t& on_path_param) const
+{
+    return std::shared_ptr<const node>(const_cast<router*>(this)->find(path, on_path_param));
+}
+
+
+template <typename T>
+std::shared_ptr<typename router<T>::node> router<T>::node::insert(std::string_view path, std::shared_ptr<node> other)
 {
     const char* begin = path.size() > 0 ? path.data() + 1 : path.data();
     const char* end = find_first_of(begin, "/?");
     std::string next_word(begin, end - begin);
 
-    auto it = _children.find(next_word);
+    auto it = children.find(next_word);
 
-    if (it == std::end(_children))
-        it = _children.emplace(next_word, std::make_shared<node>()).first;
+    if (it == std::end(children))
+        it = children.emplace(next_word, std::make_shared<node>()).first;
     
     if (*end == 0 || *end == '?')
         return std::exchange(it->second, other);
 
-    return it->second->mount(end, other);
+    return it->second->insert(end, other);
 }
 
 template <typename T>
-std::shared_ptr<typename router<T>::node> router<T>::node::unmount(std::string_view path)
+std::shared_ptr<typename router<T>::node> router<T>::node::remove(std::string_view path)
 {
     const char* begin = path.size() > 0 ? path.data() + 1 : path.data();
     const char* end = find_first_of(begin, "/?");
     std::string next_word(begin, end - begin);
 
-    auto it = _children.find(next_word);
+    auto it = children.find(next_word);
 
-    if (it == std::end(_children))
+    if (it == std::end(children))
         return {};
-
+        
     if (*end == 0 || *end == '?')
     {
-        auto tmp = it->second;
-        _children.erase(it);
-        return tmp;
+        auto _node = it->second;
+        children.erase(it);
+        return _node;
     }
 
-    return it->second->unmount(end);
+    return it->second->remove(end);
 }
 
 template <typename T>
-const char* router<T>::find_first_of(const char* data, const char* chars)
+template <bool IsConst>
+router<T>::basic_iterator<IsConst>::basic_iterator() = default;
+
+template <typename T>
+template <bool IsConst>
+router<T>::basic_iterator<IsConst>::basic_iterator(std::nullptr_t) : basic_iterator() {}
+
+template <typename T>
+template <bool IsConst>
+typename router<T>::basic_iterator<IsConst>::ref_t router<T>::basic_iterator<IsConst>::operator*() const
+{
+    return _node->value;
+}
+
+template <typename T>
+template <bool IsConst>
+typename router<T>::basic_iterator<IsConst>::ptr_t router<T>::basic_iterator<IsConst>::operator->() const
+{
+    return &_node->value;
+}
+
+template <typename T>
+template <bool IsConst>
+router<T>::basic_iterator<IsConst>::operator bool() const
+{
+    return _node != nullptr;
+}
+
+template <typename T>
+template <bool IsConst>
+bool router<T>::basic_iterator<IsConst>::operator==(const basic_iterator& other) const
+{
+    return _node == other._node;
+}
+
+template <typename T>
+template <bool IsConst>
+bool router<T>::basic_iterator<IsConst>::operator!=(const basic_iterator& other) const
+{
+    return !(*this == other);
+}
+
+template <typename T>
+template <bool IsConst>
+router<T>::basic_iterator<IsConst>::basic_iterator(std::shared_ptr<node_t> _node) : _node{_node} {}
+
+template <typename HandlerT>
+const char* router<HandlerT>::find_first_of(const char* data, const char* chars)
 {
     for (; *data; ++data)
         for (const char* ch = chars; *ch; ++ch)
@@ -199,49 +274,59 @@ const char* router<T>::find_first_of(const char* data, const char* chars)
 }
 
 template <typename T>
-router<T>::router(std::shared_ptr<node> root) : _root{root} {}
-
-template <typename T>
 router<T>::router() = default;
 
 template <typename T>
-std::optional<typename router<T>::value_t>& router<T>::at(std::string_view path)
+router<T>::router(iterator it) : _root{it._node} {}
+
+template <typename T>
+typename router<T>::iterator router<T>::insert(std::string_view path)
 {
     if (empty())
         _root = std::make_shared<node>();
     if (path == "/")
-        return _root->_value;
-    return _root->at(path)->_value;
+        return _root;
+    return _root->insert(path);
 }
 
 template <typename T>
-const std::optional<typename router<T>::value_t>& router<T>::at(std::string_view path, const on_path_param_t& on_path_param) const
+typename router<T>::iterator router<T>::find(std::string_view path, const on_path_param_t& on_path_param)
 {
-    // if (empty())
-    //     return {};
-    if (path == "/")
-        return _root->_value;
-    return _root->at(path, on_path_param)->_value;
+    if (empty())
+        return {};
+    if (path == "/" && _root->value)
+        return _root;
+    return _root->find(path, on_path_param);
 }
 
 template <typename T>
-router<T> router<T>::mount(std::string_view path, router other)
+typename router<T>::const_iterator router<T>::find(std::string_view path, const on_path_param_t& on_path_param) const
+{
+    if (empty())
+        return {};
+    if (path == "/" && root->value)
+        return std::shared_ptr<const node>(_root);
+    return std::shared_ptr<const node>(_root)->find(path, on_path_param);
+}
+
+template <typename T>
+typename router<T>::iterator router<T>::insert(std::string_view path, iterator other)
 {
     if (path == "/")
-        return std::exchange(_root, other._root);
+        return std::exchange(_root, other._node);
     if (empty())
         _root = std::make_shared<node>();
-    return _root->mount(path, other._root);
+    return _root->insert(path, other._node);
 }
 
 template <typename T>
-router<T> router<T>::unmount(std::string_view path)
+typename router<T>::iterator router<T>::remove(std::string_view path)
 {
     if (path == "/")
         return std::exchange(_root, {});
     if (empty())
         return {};
-    return _root->unmount(path);
+    return _root->remove(path);
 }
 
 template <typename T>
@@ -250,9 +335,20 @@ bool router<T>::contains(std::string_view path) const
     if (empty())
         return false;
     if (path == "/")
-        return _root && _root->_value;
-    auto _node = _root->at(path);
-    return _node && _node->_value;
+        return _root->value.has_value();
+    return _root->find(path) != nullptr;
+}
+
+template <typename T>
+typename router<T>::iterator router<T>::root()
+{
+    return _root;
+}
+
+template <typename T>
+typename router<T>::const_iterator router<T>::root() const
+{
+    return _root;
 }
 
 template <typename T>
@@ -262,13 +358,13 @@ void router<T>::clear()
 }
 
 template <typename T>
-bool router<T>::empty() const 
+bool router<T>::empty() const
 {
-    return !_root;
+    return _root == nullptr;
 }
 
 template <typename T>
-router<T>::operator bool() const 
+router<T>::operator bool() const
 {
     return !empty();
 }
--- tests/router_tests.cpp
+++ tests/router_tests.cpp
@@ -10,10 +10,10 @@ TEST(RouterTest, AddRootHandler)
 {
     router<std::string> r;
 
-    r.at("/") = "handler";
+    *r.insert("/") = "handler";
 
     std::string handler;
-    handler = *r.at("/");
+    handler = **r.find("/");
 
     EXPECT_EQ(handler, "handler");
 }
@@ -24,23 +24,24 @@ TEST(RouterTest, AddHandler)
 {
     router<std::string> r;
 
-    r.at("/home") = "handler";
+    *r.insert("/home") = "handler";
 
     std::string handler;
-    handler = *r.at("/home");
+    handler = **r.find("/home");
 
     EXPECT_EQ(handler, "handler");
 }
+
 
 TEST(RouterTest, ContainsRootHandler)
 {
     router<std::string> r;
 
-    r.at("/") = "handler";
+    *r.insert("/") = "handler";
 
     EXPECT_TRUE(r.contains("/"));
 
-    r.unmount("/");
+    r.remove("/");
 
     EXPECT_FALSE(r.contains("/"));
 }
@@ -51,11 +52,11 @@ TEST(RouterTest, ContainsHandler)
 {
     router<std::string> r;
 
-    r.at("/home") = "handler";
+    *r.insert("/home") = "handler";
 
     EXPECT_TRUE(r.contains("/home"));
 
-    r.unmount("/home");
+    r.remove("/home");
 
     EXPECT_FALSE(r.contains("/home"));
 }
@@ -65,14 +66,15 @@ TEST(RouterTest, ContainsHandler)
 TEST(RouterTest, MountRoot)
 {
     router<std::string> r;
+
     router<std::string> m;
     
-    m.at("/home") = "handler";
+    *m.insert("/home") = "handler";
 
-    auto res = r.mount("/", m);
+    auto res = r.insert("/", m.root());
 
     std::string handler;
-    handler = *r.at("/home");
+    handler = **r.find("/home");
 
     EXPECT_EQ(handler, "handler");
 }
@@ -84,16 +86,16 @@ TEST(RouterTest, Mount)
 {
     router<std::string> r;
 
-    r.at("/home") = "handler";
+    *r.insert("/home") = "handler";
 
     router<std::string> m;
     
-    m.at("/") = "auth";
+    *m.insert("/") = "auth";
 
-    auto res = r.mount("/home/auth", m);
+    auto res = r.insert("/home/auth", m.root());
 
     std::string handler;
-    handler = *r.at("/home/auth");
+    handler = **r.find("/home/auth");
 
     EXPECT_EQ(handler, "auth");
 }
@@ -104,17 +106,19 @@ TEST(RouterTest, UnmountRoot)
 {
     router<std::string> r;
 
-    r.at("/") = "handler";
+    *r.insert("/") = "handler";
 
-    router<std::string> m = r.unmount("/");
+    router<std::string> m = r.remove("/");
 
     std::string handler;
 
-    EXPECT_FALSE(r.contains("/"));
+    auto it = r.find("/");
+    EXPECT_EQ(it, nullptr);
 
-    handler = *m.at("/");
+    handler = **m.find("/");
     EXPECT_EQ(handler, "handler");
 }
+
 
 
 
@@ -122,54 +126,55 @@ TEST(RouterTest, Unmount)
 {
     router<std::string> r;
 
-    r.at("/home") = "handler";
+    *r.insert("/home") = "handler";
 
-    router<std::string> m = r.unmount("/home");
+    router<std::string> m = r.remove("/home");
 
     std::string handler;
 
-    EXPECT_FALSE(r.contains("/home"));
+    auto it = r.find("/home");
+    EXPECT_EQ(it, nullptr);
 
-    handler = *m.at("/");
+    handler = **m.find("/");
     EXPECT_EQ(handler, "handler");
 }
 
 
-TEST(RouterTest, UseCase)
+TEST(RouterTest, Goddamn)
 {
     router<std::string> api;
 
-    api.at("/service/auth/test") = "TEST";
-    api.at("/service/auth") = "ROOT";
+    *api.insert("/service/auth/test") = "TEST";
+    *api.insert("/service/auth") = "ROOT";
 
     router<std::string> auth;
 
-    auth.at("/") = "AUTH";
-    auth.at("/login") = "AUTH_LOGIN";
-    auth.at("/register") = "AUTH_REGISTER";
+    *auth.insert("/") = "AUTH";
+    *auth.insert("/login") = "AUTH_LOGIN";
+    *auth.insert("/register") = "AUTH_REGISTER";
 
-    router<std::string> r = api.mount("/service/auth", auth);
+    router<std::string> r = api.insert("/service/auth", auth.root());
 
     std::string handler;
 
     handler = "";
-    handler = *api.at("/service/auth");
+    handler = **api.find("/service/auth");
     EXPECT_EQ(handler, "AUTH");
 
     handler = "";
-    handler = *api.at("/service/auth/login");
+    handler = **api.find("/service/auth/login");
     EXPECT_EQ(handler, "AUTH_LOGIN");
 
     handler = "";
-    handler = *api.at("/service/auth/register");
+    handler = **api.find("/service/auth/register");
     EXPECT_EQ(handler, "AUTH_REGISTER");
 
     handler = "";
-    handler = *r.at("/");
+    handler = **r.find("/");
     EXPECT_EQ(handler, "ROOT");
 
     handler = "";
-    handler = *r.at("/test");
+    handler = **r.find("/test");
     EXPECT_EQ(handler, "TEST");
 }
 
@@ -177,7 +182,7 @@ TEST(RouterTest, PathParams)
 {
     router<std::string> r;
 
-    r.at("/home/:user_id/smth") = "handler";
+    *r.insert("/home/:user_id/smth") = "handler";
 
     std::string handler;
 
@@ -186,7 +191,7 @@ TEST(RouterTest, PathParams)
     {
         user_id = {std::string(key), std::string(value)};
     };
-    handler = *r.at("/home/42/smth", on_path_param);
+    handler = **r.find("/home/42/smth", on_path_param);
     EXPECT_EQ(handler, "handler");
     EXPECT_EQ(user_id.first, "user_id");
     EXPECT_EQ(user_id.second, "42");
@@ -198,7 +203,7 @@ TEST(RouterTest, ManyPathParams)
 {
     router<std::string> r;
 
-    r.at("/a/b/c/:param1/d/:param2") = "handler";
+    *r.insert("/a/b/c/:param1/d/:param2") = "handler";
 
     std::string handler;
 
@@ -209,7 +214,7 @@ TEST(RouterTest, ManyPathParams)
         path_params[std::string(key)] = std::string(value);
     };
     
-    handler = *r.at("/a/b/c/37/d/42", on_path_param);
+    handler = **r.find("/a/b/c/37/d/42", on_path_param);
     EXPECT_EQ(handler, "handler");
     EXPECT_EQ(path_params.size(), 2);
     EXPECT_EQ(path_params["param1"], "37");
@@ -220,9 +225,9 @@ TEST(RouterTest, Ambiguous)
 {
     router<std::string> r;
 
-    r.at("/hello/:a/world") = "handler1";
-    r.at("/hello/:b/world/:c") = "handler2";
-    r.at("/hello/:d/world/:e/:f") = "handler3";
+    *r.insert("/hello/:a/world") = "handler1";
+    *r.insert("/hello/:b/world/:c") = "handler2";
+    *r.insert("/hello/:d/world/:e/:f") = "handler3";
 
     std::string handler;
 
@@ -233,14 +238,14 @@ TEST(RouterTest, Ambiguous)
         path_params[std::string(key)] = std::string(value);
     };
     
-    handler = *r.at("/hello/100/world", on_path_param);
+    handler = **r.find("/hello/100/world", on_path_param);
     EXPECT_EQ(handler, "handler1");
     EXPECT_EQ(path_params.size(), 1);
     EXPECT_EQ(path_params["a"], "100");
 
     handler = "";
     path_params.clear();
-    handler = *r.at("/hello/200/world/300", on_path_param);
+    handler = **r.find("/hello/200/world/300", on_path_param);
     EXPECT_EQ(handler, "handler2");
     EXPECT_EQ(path_params.size(), 2);
     EXPECT_EQ(path_params["b"], "200");
@@ -249,7 +254,7 @@ TEST(RouterTest, Ambiguous)
 
     handler = "";
     path_params.clear();
-    handler = *r.at("/hello/400/world/500/600", on_path_param);
+    handler = **r.find("/hello/400/world/500/600", on_path_param);
     EXPECT_EQ(handler, "handler3");
     EXPECT_EQ(path_params.size(), 3);
     EXPECT_EQ(path_params["d"], "400");
@@ -263,7 +268,7 @@ TEST(RouterTest, Wildcard)
 {
     router<std::string> r;
 
-    r.at("/home/*wildcard?") = "handler";
+    *r.insert("/home/*wildcard?") = "handler";
 
     std::string handler;
 
@@ -272,7 +277,7 @@ TEST(RouterTest, Wildcard)
     {
         wildcard = {std::string(key), std::string(value)};
     };
-    handler = *r.at("/home/long/path/to/smth", on_path_param);
+    handler = **r.find("/home/long/path/to/smth", on_path_param);
     EXPECT_EQ(handler, "handler");
     EXPECT_EQ(wildcard.first, "wildcard");
     EXPECT_EQ(wildcard.second, "long/path/to/smth");
@@ -284,7 +289,7 @@ TEST(RouterTest, WildcardRoot)
 {
     router<std::string> r;
 
-    r.at("/*wildcard") = "handler";
+    *r.insert("/*wildcard") = "handler";
 
     std::string handler;
 
@@ -293,7 +298,7 @@ TEST(RouterTest, WildcardRoot)
     {
         wildcard = {std::string(key), std::string(value)};
     };
-    handler = *r.at("/home/long/path/to/smth", on_path_param);
+    handler = **r.find("/home/long/path/to/smth", on_path_param);
     EXPECT_EQ(handler, "handler");
     EXPECT_EQ(wildcard.first, "wildcard");
     EXPECT_EQ(wildcard.second, "home/long/path/to/smth");
@@ -303,8 +308,8 @@ TEST(RouterTest, WildcardBetween)
 {
     router<std::string> r;
 
-    r.at("/hello/world") = "Hello, World!";
-    r.at("/hello/*wildcard") = "handler";
+    *r.insert("/hello/world") = "Hello, World!";
+    *r.insert("/hello/*wildcard") = "handler";
 
     std::string handler;
 
@@ -313,7 +318,7 @@ TEST(RouterTest, WildcardBetween)
     {
         wildcard = {std::string(key), std::string(value)};
     };
-    handler = *r.at("/hello/world/long/path/to/smth", on_path_param);
+    handler = **r.find("/hello/world/long/path/to/smth", on_path_param);
     EXPECT_EQ(handler, "handler");
     EXPECT_EQ(wildcard.first, "wildcard");
     EXPECT_EQ(wildcard.second, "world/long/path/to/smth");
